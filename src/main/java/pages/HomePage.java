a54553bf9765b1b17517c4bb6d05aac69ca34293
dicespedes <deo.cespedes@gmail.com>
2021-09-02 08:38:13 -0600

    Segundo ejemplo agregar codigo

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,11 +6,10 @@ import org.openqa.selenium.WebElement;
 public class HomePage {
     private WebDriver driver;
 
-
-
     public HomePage(WebDriver driver){
         this.driver = driver;
     }
+
     public LoginPage clickFormAuthentication(){
         clickLink("Form Authentication");
     return new LoginPage(driver);
@@ -18,6 +17,10 @@ public class HomePage {
     public DropdownPage clickDropdown(){
         clickLink("Dropdown");
         return new DropdownPage(driver);
+    }
+    public DynamicLoadingPage clickDynamicLoading(){
+        clickLink("Dynamic Loading");
+        return new DynamicLoadingPage(driver);
     }
     private void clickLink(String linkText){
         driver.findElement(By.linkText(linkText)).click();
